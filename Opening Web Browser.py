c510e712590da36fe2521a59ebd59a200f14dd8c
patidarparas13 <patidarparas13@gmail.com>
2018-12-12 15:18:55 +0530

    New File Change

--- Opening Web Browser.py
+++ Opening Web Browser.py
@@ -6,5 +6,5 @@ break_count = 0
 
 while(break_count < total_breaks):
     time.sleep(1)
-    webbrowser.open("http://www.youtube.com/watch?v=YXw16RzMofo")
+    webbrowser.open("https://www.youtube.com/channel/UCFBKSRHPBO8NGN_2AmliCoQ?view_as=subscriber")
     break_count = break_count + 1
